c3647105aca4c9cbba8aeccb6dd8759329e37fdf
githMedinaGto <89074060+githMedinaGto@users.noreply.github.com>
2023-07-02 01:29:49 -0600

    Se agrega la coneción a partir de Entity Framework y los modelos que representaran cada tabla

---
+++ App_Data/Models/Estado/EstadoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSecureBookings
+{
+    public class EstadoModel
+    {
+        public int idEstado { get; set; }
+        public int idMunicipio { get; set; }
+	    public string sEstado { get; set; }
+
+        public virtual MunicipioModel Municipio { get; set; }
+    }
+}
---
+++ App_Data/Models/Rol/RolModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSecureBookings
+{
+    public class RolModel
+    {
+        public int idRol { get; set; }
+        public string sNomRol { get; set; }
+        public string sDescripcion { get; set; } = null;
+    }
+}
