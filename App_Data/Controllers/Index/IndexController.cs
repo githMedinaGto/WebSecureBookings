65554eeab7b7189287da838e7cdd24d8e67e574b
githMedinaGto <89074060+githMedinaGto@users.noreply.github.com>
2023-07-04 12:48:53 -0600

    Se agrega el ejemplo del CRUD con el uso de entity framework

--- App_Data/Controllers/Index/IndexController.cs
+++ App_Data/Controllers/Index/IndexController.cs
@@ -1,91 +1,262 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.Services.Description;
+using WebSecureBookings.App_Data.Models;
 
 namespace WebSecureBookings.Controllers.IndexController
 {
     public class IndexController
     {
 
-        #region Obtener la tabla dejugadores
-        [HttpPost]
-        public List<string> TablaEquipos()
+        public ResponseModel<List<RolModel>> GetRolDataTable()
         {
-            List<string> resultados = new List<string>();
-            string sTabla = "";
-            string sIdTabla = "";
-            string[] aColumnas = new string[] { "NOMBRE", "Color", "Código", "Liga", "Deporte", "Director Técnico", "ACCIONES" };
-            string query = "SELECT [idEquipo], te.sNombre, [sColor], [sCodigoEquipo], tl.sNombre, td.sNombre, sNomDirTec" +
-                "\r\nFROM [tEquipo] as te" +
-                "\r\nInner Join [tLiga] tl ON te.iIdLiga = tl.idLiga" +
-                "\r\nInner Join [tDeporte] td ON tl.iIdDeporte = td.idDeporte";
-            DataTable dtDatos = new DataTable();
+            
             try
             {
-                Connection cn = new Connection();
-                dtDatos = cn.ExecuteQueryDataTable(query);
-                sIdTabla = "tblEquipos";
-
-                if (dtDatos != null && dtDatos.Rows.Count > 0)
+                using (var dbContext = new DB_WSBEntities())
                 {
-                    sTabla += "<table id='" + sIdTabla + "' class='table table-striped table-bordered table-hover dataTable' style='width: 100%'> " +
-                        "<thead>" +
-                        "<tr>";
-                    foreach (string sColumna in aColumnas)
-                    {
-                        sTabla += "<th>" + sColumna + "</th>";
-                    }
-                    sTabla += "</tr></thead>";
-                    sTabla += "<tbody>";
-                    foreach (DataRow oRow in dtDatos.Rows)
-                    {
-                        sTabla += "<tr>";
-                        sTabla += "<td>" + oRow[1].ToString() + "</td>";
-                        sTabla += "<td>" + oRow[2].ToString() + "</td>";
-                        sTabla += "<td>" + oRow[3].ToString() + "</td>";
-                        sTabla += "<td>" + oRow[4].ToString() + "</td>";
-                        sTabla += "<td>" + oRow[5].ToString() + "</td>";
-                        sTabla += "<td>" + oRow[6].ToString() + "</td>";
+                    var lst = (from d in dbContext.tRol
+                               select new RolModel
+                               {
+                                   idRol = d.idRol,
+                                   sNomRol = d.sNomRol,
+                                   sDescripcion = d.sDescripcion
+                               }).ToList();
 
-                        sTabla += "<td>" +
-                            "<span class='fa fa-pencil-square-o' style='color:#85c555;font-size: 35px;  cursor: pointer;' onclick='javascript:fn_EditEquipo(" + oRow[0].ToString() + ");'></span>" +
-                            "&nbsp;&nbsp;" +
-                            "<i class=\"fa fa-duotone fa-trash-can\" style='color:red;font-size: 35px;  cursor: pointer;' onclick='javascript:fn_EliminarEquipo(" + oRow[0].ToString() + ");'></i>" +
-                            "</td>";
-                        sTabla += "</tr>";
-                    }
-                    sTabla += "</tbody>";
-                    sTabla += "<tfoot>" +
-                        "<tr>";
-                    sTabla += "</tr>" +
-                        "</tfoot>" +
-                        "</table>";
-
-                    resultados.Add("200"); // Código de estado
-                    resultados.Add("OK"); // Texto del estado
-                    resultados.Add(sTabla); // Contenido de la tabla
-                }
-                else
-                {
-                    resultados.Add("404"); // Código de estado
-                    resultados.Add("No se encontraron registros"); // Texto del estado
-                    resultados.Add(string.Empty); // Contenido de la tabla vacío
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 200,
+                        Message = "Datos obtenidos correctamente",
+                        Data = lst
+                    };
                 }
             }
             catch (Exception ex)
             {
-                resultados.Add("500"); // Código de estado
-                resultados.Add("Error interno del servidor"); // Texto del estado
-                resultados.Add(string.Empty); // Contenido de la tabla vacío
+                // Manejo de errores específicos y retorno de la respuesta personalizada
+                if (ex is SqlException)
+                {
+                    // Error específico de SQL
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 500,
+                        Message = "Error en la base de datos: " + ex.Message,
+                        Data = null
+                    };
+                }
+                else
+                {
+                    // Otros errores
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 404,
+                        Message = "No se pudo obtener los datos de la base de datos: " + ex.Message,
+                        Data = null
+                    };
+                }
             }
-
-            return resultados;
         }
-        #endregion
+
+        [HttpPost]
+        public ResponseModel<string> PostCrearRol(string sRol, string sDescripcion)
+        {
+            
+            try
+            {
+                using (var dbContext = new DB_WSBEntities())
+                {
+                    var oRol = new tRol();
+                    oRol.sNomRol = sRol;
+                    oRol.sDescripcion = sDescripcion;
+
+                    dbContext.tRol.Add(oRol);
+                    dbContext.SaveChanges();
+                }
+
+                return new ResponseModel<string>
+                {
+                    StatusCode = 200,
+                    Message = "Datos guardados exitosamente"
+                };
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores específicos y retorno de la respuesta personalizada
+                if (ex is SqlException)
+                {
+                    // Error específico de SQL
+                    return new ResponseModel<string>
+                    {
+                        StatusCode = 500,
+                        Message = "Error en la base de datos: " + ex.Message
+                    };
+                }
+                else
+                {
+                    // Otros errores
+                    return new ResponseModel<string>
+                    {
+                        StatusCode = 404,
+                        Message = "No se pudo guardar los datos de la base de datos: " + ex.Message
+                    };
+                }
+            }
+        }
+
+        [HttpGet]
+        public ResponseModel<List<RolModel>> GetRolId(string idRol) {
+            try
+            {
+                RolModel model= new RolModel();
+                using (var dbContext = new DB_WSBEntities())
+                {
+                    int iIdROl = int.Parse(idRol); // Convertir idRol a int
+                    var oRol = dbContext.tRol.Find(iIdROl);
+                    model.idRol = oRol.idRol;
+                    model.sNomRol = oRol.sNomRol;
+                    model.sDescripcion = oRol.sDescripcion;
+
+                    // Crear una lista que contenga la instancia del modelo
+                    List<RolModel> lst = new List<RolModel>();
+                    lst.Add(model);
+
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 200,
+                        Message = "Datos obtenidos correctamente",
+                        Data = lst
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores específicos y retorno de la respuesta personalizada
+                if (ex is SqlException)
+                {
+                    // Error específico de SQL
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 500,
+                        Message = "Error en la base de datos: " + ex.Message,
+                        Data = null
+                    };
+                }
+                else
+                {
+                    // Otros errores
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 404,
+                        Message = "No se pudo obtener los datos de la base de datos: " + ex.Message,
+                        Data = null
+                    };
+                }
+            }
+        }
+
+        [HttpPut]
+        public ResponseModel<string> PutRol(string idRol,string sRol, string sDescripcion)
+        {
+
+            try
+            {
+                using (var dbContext = new DB_WSBEntities())
+                {
+                    int iIdROl = int.Parse(idRol); // Convertir idRol a int
+                    var oRol = dbContext.tRol.Find(iIdROl);
+
+                    //var oRol = new tRol();
+                    oRol.sNomRol = sRol;
+                    oRol.sDescripcion = sDescripcion;
+                    oRol.idRol = iIdROl;
+
+                    dbContext.Entry(oRol).State = System.Data.Entity.EntityState.Modified;
+                    dbContext.SaveChanges();
+                }
+
+                return new ResponseModel<string>
+                {
+                    StatusCode = 200,
+                    Message = "Datos actualizados exitosamente"
+                };
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores específicos y retorno de la respuesta personalizada
+                if (ex is SqlException)
+                {
+                    // Error específico de SQL
+                    return new ResponseModel<string>
+                    {
+                        StatusCode = 500,
+                        Message = "Error en la base de datos: " + ex.Message
+                    };
+                }
+                else
+                {
+                    // Otros errores
+                    return new ResponseModel<string>
+                    {
+                        StatusCode = 404,
+                        Message = "No se pudo guardar los datos de la base de datos: " + ex.Message
+                    };
+                }
+            }
+        }
+
+        [HttpDelete]
+        public ResponseModel<List<RolModel>> DeleteRolId(string idRol)
+        {
+            try
+            {
+                using (var dbContext = new DB_WSBEntities())
+                {
+                    int iIdROl = int.Parse(idRol); // Convertir idRol a int
+                    var oRol = dbContext.tRol.Find(iIdROl);
+                    dbContext.tRol.Remove(oRol);
+                    dbContext.SaveChanges();
+
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 200,
+                        Message = "Datos eliminados correctamente",
+                        Data = null
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores específicos y retorno de la respuesta personalizada
+                if (ex is SqlException)
+                {
+                    // Error específico de SQL
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 500,
+                        Message = "Error en la base de datos: " + ex.Message,
+                        Data = null
+                    };
+                }
+                else
+                {
+                    // Otros errores
+                    return new ResponseModel<List<RolModel>>
+                    {
+                        StatusCode = 404,
+                        Message = "No se pudo obtener los datos de la base de datos: " + ex.Message,
+                        Data = null
+                    };
+                }
+            }
+        }
     }
 
 }
