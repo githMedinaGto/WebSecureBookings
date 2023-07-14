f4b08102a18e683df7eddd8eeda3e78048047376
spalomino13 <samupalomino13@gmail.com>
2023-07-14 00:21:21 -0600

    Se sube la creacion del registro de usuarios
    Se creo la pantalla de registro de usuarios(profesionistas y clientes) junto con la funcionalidad de poder registrarse como el usuario lo desee.

--- App_Data/Controllers/PerfilesProfesionistas/PerfilesProfesionistasController.cs
+++ App_Data/Controllers/PerfilesProfesionistas/PerfilesProfesionistasController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using WebSecureBookings.App_Data.Models;
 
