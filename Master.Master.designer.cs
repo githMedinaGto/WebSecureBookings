91b65088b4559f8d645124e9f51a7d1b7ebb3583
githMedinaGto <racieriver@gmail.com>
2023-08-15 22:32:35 -0600

    Se agrega la funcionalidad de generar el menu y la validación del token no se a expirado para regresar a la pagina de logueo

--- Master.Master.designer.cs
+++ Master.Master.designer.cs
@@ -24,6 +24,15 @@ namespace WebSecureBookings
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
+        /// Control menu.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menu;
+
+        /// <summary>
         /// Control body.
         /// </summary>
         /// <remarks>
